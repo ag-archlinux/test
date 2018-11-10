ae78b82bdcbf4bc44d7f62a9f8e41d50ef79ea09
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 17:46:20 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
-ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the internet!" || echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m" && wifi-menu
+ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the internet!" || echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m" 
+ping -q -w1 -c1 google.com &>/dev/null || wifi-menu
+ping -q -w1 -c1 google.com &>/dev/null || kill -s SIGINT <PID>
 ##### TEST      ######
 read -p "Press any key..."
 #####  SELECT THE MIRRORS          #####
+NEXT=0
+while[$NEXT=1]
+do
+read -p "Reboot (y/n)?" ANSWER
+case "$ANSWER" in 
+  y|Y ) reboot
+        $NEXT=1     
+  ;;
+  n|N ) $NEXT=1
+  ;;
+esac
+done
+
+#while [ -z "$ROOT_PASSWORD" ]
+#do
+#    read -p "Continue (y/n)?" choice
+#
+#    if [ "$PASSWORD1" = "$PASSWORD2" ]; then
+#        ROOT_PASSWORD=$PASSWORD1
+#    else
+#        color1='\033[0;36m'
+#        color2='\033[0;0m'
+#        echo ""
+#        echo -e "${color1}Passwords did not match!${color2}"
+#    fi
+#done
 pacman --noconfirm --needed -S reflector
 
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
