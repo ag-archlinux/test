fc11baf80d341f37880f55e92f0efc8c3fb31507
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 19:42:34 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-[ping -q -w1 -c1 google.com &>/dev/null] && {echo "You are connected to the internet!";} || {echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit;}
 ##### TEST      ######
+[ping -q -w1 -c1 google.com &>/dev/null] && {echo "You are connected to the internet!";} || {echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit;}
 #####  SELECT THE MIRRORS          #####
 question_yesno(){
 NEXT=0
@@ -23,6 +23,3 @@ question_yesno "Reboot (y/n)?" reboot
 #pacman --noconfirm --needed -S reflector
 #nano /etc/pacman.d/mirrorlist  
 #sudo pacman -Syy
-
-
-
