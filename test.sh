d4215f710d393c1b035c62f537b75609931f8b33
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 23:40:15 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 set -e
 ##### TEST      ######
-ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the internet!" || (echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit;)
-#####  QUESTION YESNO              #####        
-question_yesno(){
-NEXT=0; until [ $NEXT -eq 1 ]; do
-  read -p "$1" ANSWER
-  case "$ANSWER" in 
-    [yY][eE][sS]|[yY]) 
-      $2
-      NEXT=1
-      ;;
-    [nN][oO]|[nN]) 
-      $3
-      NEXT=1
-      ;;
-  esac
-done
-}
-question_yesno "Reboot (y/n)? " "reboot" ""
+
 #####  SELECT THE MIRRORS          #####
-#pacman --noconfirm --needed -S reflector
-#nano /etc/pacman.d/mirrorlist  
-#sudo pacman -Syy
+
+sudo pacman -S --noconfirm --needed reflector
+
+sudo reflector -l 100 -f 50 --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
+
+cat /etc/pacman.d/mirrorlist
+
+sudo pacman -Syu
+
+
 
 
 
@@ -42,24 +32,3 @@ question_yesno "Reboot (y/n)? " "reboot" ""
 #        echo -e "${color1}Passwords did not match!${color2}"
 #    fi
 #done
-
-
-#!/bin/bash
-#####  FUNCTIONS SCRIPT                        #####
-set -e
-#####  QUESTION YES/NO FUNCTION                #####        
-question_yesno(){
-NEXT=0; until [ $NEXT -eq 1 ]; do
-  read -p "$1" ANSWER
-  case "$ANSWER" in 
-    [yY][eE][sS]|[yY]) 
-      $2
-      NEXT=1
-      ;;
-    [nN][oO]|[nN]) 
-      $3
-      NEXT=1
-      ;;
-  esac
-done
-}
