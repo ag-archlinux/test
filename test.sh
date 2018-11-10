d4298ca34821af55c47dfdc4f526cf983d1fca86
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 03:59:27 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 ##### TEST      ######
 #####  SELECT THE MIRRORS          #####
-pacman --noconfirm --needed -S reflector rsync
+pacman --noconfirm --needed -S reflector
+
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+
+#sudo reflector -l 100 -f 50 -c BE -c NL -c FR -c DE -c GB --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new
+#rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist 
+#sudo cp /tmp/mirrorlist /etc/pacman.d
+
+sudo reflector -l 100 -f 50 --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new
+rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist
+sudo cp /tmp/mirrorlist /etc/pacman.d
+
+
 cat /etc/pacman.d/mirrorlist
-read -p "Press any key..."
 sudo pacman -Syu
-dialog --defaultno --title "=>" --yesno "Reboot computer?"  5 30 && reboot
-dialog --defaultno --title "=>" --yesno "Return to chroot environment?"  6 30 && arch-chroot /mnt
