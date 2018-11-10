1bb1a3b06b725b6bb5b64683be8dba61180d6a20
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 21:44:05 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,7 @@
 ##### TEST      ######
 ping -q -w1 -c1 google.com &>/dev/null && (echo "You are connected to the internet!";PIN=1;) || (echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;PIN=0;)
 if [ $PIN = 0 ]; then exit
-#####  SELECT THE MIRRORS          #####
-#pacman --noconfirm --needed -S reflector
-#nano /etc/pacman.d/mirrorlist  
-#sudo pacman -Syy
+#####  QUESTION YESNO              #####        
 question_yesno(){
 NEXT=0; until [ $NEXT -eq 1 ]; do
   read -p "$1" ANSWER
@@ -22,3 +19,7 @@ NEXT=0; until [ $NEXT -eq 1 ]; do
 done
 }
 question_yesno "Reboot (y/n)?" "reboot" ""
+#####  SELECT THE MIRRORS          #####
+#pacman --noconfirm --needed -S reflector
+#nano /etc/pacman.d/mirrorlist  
+#sudo pacman -Syy
