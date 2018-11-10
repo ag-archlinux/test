b23ec095521f8b288b7b77c9522a5d5acd82ce77
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 04:10:36 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,14 +5,9 @@ pacman --noconfirm --needed -S reflector
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
-#sudo reflector -l 100 -f 50 -c BE -c NL -c FR -c DE -c GB --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new
-#rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist 
-#sudo cp /tmp/mirrorlist /etc/pacman.d
-
-sudo reflector -l 100 -f 50 --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new
-rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist
+sudo reflector -l 100 -f 50 -c SK -c CZ -c PL -c DE -c GB --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new
+rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist 
 sudo cp /tmp/mirrorlist /etc/pacman.d
-
 
 cat /etc/pacman.d/mirrorlist
 sudo pacman -Syu
