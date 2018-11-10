90c271c6b649ba93b9513a43f9f304a6bbf60639
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 20:07:14 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,12 +6,14 @@ ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the interne
 #nano /etc/pacman.d/mirrorlist  
 #sudo pacman -Syy
 question_yesno(){
-NEXT=0;
-while[$NEXT != 1]; do
+NEXT=0
+while[$NEXT -ne 1] 
+do
   read -p "$1" ANSWER
   case "$ANSWER" in 
     [yY][eE][sS]|[yY]) ($2; NEXT=1;);;
     [nN][oO]|[nN]) ($3;NEXT=1;);;
   esac
-done;}
+done
+}
 question_yesno "Reboot (y/n)?" reboot
