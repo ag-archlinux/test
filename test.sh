6124e47606d281ac7c166766d08c11fd4d9fab99
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 16:45:50 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the internet!" || echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m" && wifi-menu
 ##### TEST      ######
+read -p "Press any key..."
 #####  SELECT THE MIRRORS          #####
 pacman --noconfirm --needed -S reflector
 
