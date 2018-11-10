ffc2feacecce0c9584bc0f3db91633b4c0555244
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 19:39:00 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-if[ping -q -w1 -c1 google.com &>/dev/null] then 
-  echo "You are connected to the internet!"
-else  
-  echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit
-fi
+ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the internet!" || {echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit;}
 ##### TEST      ######
 #####  SELECT THE MIRRORS          #####
 question_yesno(){
