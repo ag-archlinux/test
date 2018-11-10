bade76a0457c0d83fbc53d869e993e3f751f1147
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 03:33:17 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ##### TEST      ######
-##### REBOOT    ######
-reboot
-#####  GO TO CHROOT ENVIRONMENT
-arch-chroot /mnt
-echo "####################     INSTALLATION FINISHED     ####################"
+#####  SELECT THE MIRRORS          #####
+pacman --noconfirm --needed -S reflector rsync
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist
+read -p "Press any key..."
+sudo pacman -Syu
+dialog --defaultno --title "=>" --yesno "Reboot computer?"  5 30 && reboot
+dialog --defaultno --title "=>" --yesno "Return to chroot environment?"  6 30 && arch-chroot /mnt
