7c3b24f8fd94da212914a85956ef3b135aea7de0
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 21:53:19 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##### TEST      ######
-ping -q -w1 -c1 google.com &>/dev/null && (echo "You are connected to the internet!";PIN=1;) || (echo >&3 -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit 1;)
-#if [ $PIN = 0 ]; then exit 1
+ping -q -w1 -c1 google.com &>/dev/null && (echo "You are connected to the internet!";PIN=1;) || (echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;PIN=0;)
+if [ $PIN = 0 ]; then exit 1
 #####  QUESTION YESNO              #####        
 question_yesno(){
 NEXT=0; until [ $NEXT -eq 1 ]; do
