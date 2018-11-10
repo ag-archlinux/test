c7f18a22689da9827e4e4e500c74ce7495809858
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 19:59:44 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 ##### TEST      ######
-ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the internet!" || (echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit)
+ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the internet!" || (echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit;)
 #####  SELECT THE MIRRORS          #####
-question_yesno "Reboot (y/n)?" reboot
 #pacman --noconfirm --needed -S reflector
 #nano /etc/pacman.d/mirrorlist  
 #sudo pacman -Syy
 question_yesno(){
-NEXT=0
+NEXT=0;
 while[$NEXT != 1]; do
   read -p "$1" ANSWER
   case "$ANSWER" in 
-    [yY][eE][sS]|[yY]) 
-      $2
-      NEXT=1     
-      ;;
-    [nN][oO]|[nN])
-      $3
-      NEXT=1
-      ;;
+    [yY][eE][sS]|[yY]) ($2; NEXT=1;);;
+    [nN][oO]|[nN]) ($3;NEXT=1;);;
   esac
-done;
-}
+done;}
+question_yesno "Reboot (y/n)?" reboot
