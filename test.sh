f01510a3950c53485db360c56b513626ec925422
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-10 15:54:18 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,6 @@ ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the interne
 #####  SELECT THE MIRRORS          #####
 pacman --noconfirm --needed -S reflector
 
-#sudo reflector -l 100 -f 50 -c BE -c NL -c FR -c DE -c GB --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
-#reflector --protocol --lastest 50 --number 20 --sort rate --save /etc/pacman.d/mirrorlist
-#reflector --country Slovakia --country Czechia --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 reflector -l 200 -f 10 --sort score --save /etc/pacman.d/mirrorlist
 cat /etc/pacman.d/mirrorlist  
 sudo pacman -Syy
