51f1a0cae0d1371cf0cdd1f7cf9cea7c5fcf2166
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-11 21:07:17 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 ##### TEST      ######
-pacman -Sy python
+sudo pacman -Sy 
+sudo pacman -S --noconfirm --needed python
 #####  SELECT THE MIRRORS          #####
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-#sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 sudo rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 sudo pacman -S --noconfirm --needed reflector
