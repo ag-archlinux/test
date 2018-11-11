b70569044513a4f191ba9ca642e7fe00e6933cb4
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-12 00:35:38 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,30 +1,84 @@
 #!/bin/bash
 set -e
-##### TEST      ######
-sudo pacman -Sy 
-sudo pacman -S --noconfirm --needed python
+#####  CHECKING CONNECTION                     #####  
+ping -q -w1 -c1 google.com &>/dev/null && echo "You are connected to the internet!" || (echo -e "\033[0;36m'You are not connected to the internet!'\033[0;0m";wifi-menu;exit;)
+#####  INPUTS                                  #####
+read -p "What is your RAM (G)? " RAM_IN
+RAM=$(bc <<< "$RAM_IN * 1.5")
+read -p "What is your ROOT_SPACE (G)? " ROOT_SPACE
+read -p "Enter your hostname: " COMPUTER_NAME
+#####  SET THE KEYBOARD LAYOUT     #####
+#####  VERIFY THE BOOT MODE        #####
+if [-d "/sys/firmware/efi/efivars"]; then
+    echo "UEFI"
+else
+    echo "BIOS"
+fi
+#####  UPDATE THE SYSTEM CLOCK     #####
+timedatectl set-ntp true
+#####  PARTITION THE DISK          #####
+cat<<EOF | fdisk /dev/sda
+o
+n
+p
+
+
++200M
+n
+p
+
+
++${RAM}G
+n
+p
+
+
++${ROOT_SPACE}G
+n
+p
+
+
+
+w
+EOF
+#####  FORMAT THE PARTITIONS       #####
+yes | mkfs.ext4 /dev/sda1
+yes | mkfs.ext4 /dev/sda3
+yes | mkfs.ext4 /dev/sda4
+mkswap /dev/sda2
+swapon /dev/sda2
+mount /dev/sda3 /mnt
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
+mkdir -p /mnt/home
+mount /dev/sda4 /mnt/home
+lsblk
+read -p "Press any key..."
 #####  SELECT THE MIRRORS          #####
-
-sudo pacman -S --noconfirm --needed reflector
-sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sudo reflector --protocol https --lastest 50 --number 20 --sort rate --save /etc/pacman.d/mirrorlist
-
-
-
-
-
-
-
-#sudo reflector -l 100 -f 50 -c BE -c NL -c FR -c DE -c GB --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
-
-#cat /etc/pacman.d/mirrorlist
-
-#sudo pacman -Syu
-
-
-
-
-
+cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+pacman -Syy
+pacman --noconfirm --needed -S reflector
+reflector --verbose -l 5 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syy
+nano /etc/pacman.d/mirrorlist
+read -p "Press any key..."
+#####  INSTAL THE BASE PACKAGES    #####
+pacstrap /mnt base base-devel
+#####  FSTAB                       #####
+genfstab -U /mnt >> /mnt/etc/fstab
+#####  MY FUNCTIONS                #####
+curl -O https://raw.githubusercontent.com/ag-archlinux/ag-archlinux/master/functions.sh && bash functions.sh
+#####  CHROOT                      #####
+curl https://raw.githubusercontent.com/ag-archlinux/ag-archlinux/master/chroot.sh > /mnt/chroot.sh 
+arch-chroot /mnt bash chroot.sh
+rm /mnt/chroot.sh
+#####  UMOUNT                      #####
+umount -R /mnt
+#####  REBOOT                      #####
+question_yesno "Reboot computer (y/n)? " "reboot" ""
+#####  GO TO CHROOT ENVIRONMENT
+question_yesno "Return to chroot environment (y/n)? " "arch-chroot /mnt" ""
+echo "####################     INSTALLATION FINISHED     ####################"
 #while [ -z "$ROOT_PASSWORD" ]
 #do
 #    echo "Please enter a root password:"
