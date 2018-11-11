2f87a46aa41e02a3fb5c8d7276415cca64ea112a
ag-archlinux <43832300+ag-archlinux@users.noreply.github.com>
2018-11-11 02:49:35 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,14 +3,25 @@ set -e
 ##### TEST      ######
 
 #####  SELECT THE MIRRORS          #####
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+sudo rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 sudo pacman -S --noconfirm --needed reflector
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sudo reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
 
-sudo reflector -l 100 -f 50 -c BE -c NL -c FR -c DE -c GB --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
 
-cat /etc/pacman.d/mirrorlist
 
-sudo pacman -Syu
+
+
+
+
+#sudo reflector -l 100 -f 50 -c BE -c NL -c FR -c DE -c GB --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
+
+#cat /etc/pacman.d/mirrorlist
+
+#sudo pacman -Syu
 
 
 
